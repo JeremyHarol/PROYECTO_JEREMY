db9813af28187ee7f881d25aae2074b8da0b316b
jeremy <jqs7054@gmai.com>
2021-08-07 20:10:45 -0500

    jeremy gormulario 1

--- PROYECTO_JEREMY/Form1.cs
+++ PROYECTO_JEREMY/Form1.cs
@@ -14,5 +14,15 @@ namespace PROYECTO_JEREMY
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
