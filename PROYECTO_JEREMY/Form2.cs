01cb621bbaa6adabd4de336aac30d8c9e70fcbd4
jeremy <jqs7054@gmai.com>
2021-08-07 20:04:48 -0500

    proyecto-jeremy-V2

---
+++ PROYECTO_JEREMY/Form2.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace PROYECTO_JEREMY
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+    }
+}
