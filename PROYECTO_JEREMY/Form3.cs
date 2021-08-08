c276e295975a4e2337519cae115904147f9cc19e
jeremy <jqs7054@gmai.com>
2021-08-07 20:23:05 -0500

    DEMO

--- PROYECTO_JEREMY/Form3.cs
+++ PROYECTO_JEREMY/Form3.cs
@@ -16,5 +16,15 @@ namespace PROYECTO_JEREMY
         {
             InitializeComponent();
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
